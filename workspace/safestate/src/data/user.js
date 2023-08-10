1be2f05d843a8f5c3305316dce9da7a0d249e514
smithd36 <drey.smith@enmu.edu>
2023-08-10 16:04:34 -0600

    Copyright fix

---
+++ workspace/safestate/src/data/user.js
@@ -0,0 +1,72 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Drey S.",
+		name: "Drey S.",
+		email: "drey@dreysmith.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/",
+		linkedin: "https://www.linkedin.com/in/drey-smith-390245224/",
+		stackoverflow: "https://stackoverflow.com/users/22170965/drey-smith",
+	},
+
+	homepage: {
+		title: "Full stack web and mobile app developer, and amateur space cowboy.",
+		description:
+			"Primarily working with the MERN Stack currently, I also have expertise in Python, Java and C++. I am experienced in  building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I'm Drey S. based in New Mexico, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "ResQCert Manager",
+			description:
+				"A full-stack Next.js tool hosted on Azure, streamlining first responder licensure. ResQCert boosts administrative efficiency, empowering healthcare providers to prioritize patient well-being. Actively enhancing this app for scalability, compliance, and security.",
+			logo: "https://github.com/smithd36/Python-SQLite-CLI-DBMS/assets/90289165/97c1d7c0-d55c-41b3-95c1-ff3931f6fc41",
+			linkText: "View Project",
+			link: "https://github.com/smithd36/ems-medical-license-manager",
+		},
+
+		{
+			title: "C++ PowerShell Toolkit",
+			description:
+				"C++ shell for managing parallel commands with seamless pipes. Redefine outputs, navigate complex workflows, and ensure tailored user experiences.",
+			logo: "https://github.com/smithd36/ems-medical-license-manager/assets/90289165/01a47ba3-be17-415a-814e-c81edb54f682",
+			linkText: "View Project",
+			link: "https://github.com/smithd36/unix-power-shell",
+		},
+
+		{
+			title: "Command Line DBMS (Python)",
+			description:
+				"A comprehensive database management system for the command line. This project was built using Python and SQLite for a group state university project. ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Personal Portfolio (this site)",
+			description:
+				"A show of my skills and expertise in computer science and development. Built with React, this dynamic portfolio elegantly presents my journey, projects, and capabilities.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
